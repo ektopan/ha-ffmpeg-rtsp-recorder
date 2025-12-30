7120cda435199aa15e110bd0fb2937eab564e69f
ektopan <50677505+ektopan@users.noreply.github.com>
2025-12-30 11:31:31 -0300

    Update run.sh

--- ffmpeg_rtsp_recorder/run.sh
+++ ffmpeg_rtsp_recorder/run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/with-contenv sh
+#!/bin/sh
 
 RTSP_URL=$(jq -r '.rtsp_url' /data/options.json)
 SEGMENT_TIME=$(jq -r '.segment_time' /data/options.json)
@@ -9,11 +9,11 @@ echo "Iniciando gravação RTSP..."
 echo "RTSP: $RTSP_URL"
 echo "Segmentos: ${SEGMENT_TIME}s"
 
-ffmpeg -rtsp_transport tcp \
--i "$RTSP_URL" \
--c copy \
--f segment \
--segment_time "$SEGMENT_TIME" \
--reset_timestamps 1 \
--strftime 1 \
-/media/cameras/a31/a31_%Y%m%d_%H%M%S.mp4
+exec ffmpeg -rtsp_transport tcp \
+  -i "$RTSP_URL" \
+  -c copy \
+  -f segment \
+  -segment_time "$SEGMENT_TIME" \
+  -reset_timestamps 1 \
+  -strftime 1 \
+  /media/cameras/a31/a31_%Y%m%d_%H%M%S.mp4
